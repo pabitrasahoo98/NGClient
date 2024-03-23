9df23581857d48dbdbaee0ea67a6395489f4d256
pabitrasahoo98 <152062702+pabitrasahoo98@users.noreply.github.com>
2024-03-23 13:46:52 +0530

    logout bug 8

--- src/components/layout/profile/Logout.jsx
+++ src/components/layout/profile/Logout.jsx
@@ -9,6 +9,7 @@ const Logout = () => {
     const navigate=useNavigate();
     useEffect(() => {
     dispatch(logout());
+    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; secure;";
 
     navigate("/");
     }, [dispatch,navigate])
